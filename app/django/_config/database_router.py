adb3d5927004066c6f5d6a8e5497178f1820bfa8
Austin Kho <kori.susie@gmail.com>
2024-12-22 15:48:05 +0900

    database router update

--- app/django/_config/database_router.py
+++ app/django/_config/database_router.py
@@ -4,23 +4,17 @@ import random
 class MasterSlaveRouter:
     @staticmethod
     def db_for_read(model, **hints):
-        """
-        읽기 작업은 slave1 또는 slave2에서 처리
-        """
+        # 읽기 작업은 slave1 또는 slave2에서 처리
         return random.choice(['slave1', 'slave2'])
 
     @staticmethod
     def db_for_write(model, **hints):
-        """
-        쓰기 작업은 master에서 처리
-        """
+        # 쓰기 작업은 default 데이터베이스에서만 처리
         return 'default'
 
     @staticmethod
     def allow_relation(obj1, obj2, **hints):
-        """
-        두 객체가 동일한 DB에 있을 때 관계 허용
-        """
+        # 두 객체가 동일한 DB에 있을 때 관계 허용
         db_list = ('default', 'slave1', 'slave2')
         if obj1._state.db in db_list and obj2._state.db in db_list:
             return True
@@ -28,7 +22,5 @@ class MasterSlaveRouter:
 
     @staticmethod
     def allow_migrate(db, app_label, model_name=None, **hints):
-        """
-        마이그레이션 작업은 master에서만 실행
-        """
+        # default 데이터베이스에서만 마이그레이션 수행
         return db == 'default'
