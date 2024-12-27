d29303ec786ab887fd18954d877e0b9e4ce9b453
Austin Kho <kori.susie@gmail.com>
2024-12-27 22:35:55 +0900

    ibs app create

--- app/django/_config/settings/base.py
+++ app/django/_config/settings/base.py
@@ -78,6 +78,7 @@ INSTALLED_APPS += [  # app
     'company.apps.CompanyConfig',
     'contract.apps.ContractConfig',
     'docs.apps.DocsConfig',
+    'ibs.apps.IbsConfig',
     'items.apps.ItemsConfig',
     'notice.apps.NoticeConfig',
     'payment.apps.PaymentConfig',
