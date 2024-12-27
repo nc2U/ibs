82f11d7381bb3353b8a6e99867368b140cb3193d
Austin Kho <kori.susie@gmail.com>
2024-12-28 00:57:53 +0900

    ibs url & view update

---
+++ app/django/ibs/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path, include
+from django.views.generic import RedirectView
+from .views import *
+
+app_name = 'ibs'
+
+urlpatterns = [
+    path('', RedirectView.as_view(url='/rebs/dashboard/'), name='home'),
+    path('dashboard/', Dashboard.as_view(), name='dashboard'),
+    path('schedule/', menu2_1, name='menu2_1'),
+    path('cash/', include('cash.urls')),
+    path('company/', include('company.urls')),
+    path('project/', include('project.urls')),
+    path('contract/', include('contract.urls')),
+    path('notice/', include('notice.urls')),
+    # path('docs/', include('document.urls')),
+]
--- app/django/ibs/views.py
+++ app/django/ibs/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from datetime import date
 
-# Create your views here.
+from django.http import HttpResponse
+from django.views import generic
+from django.shortcuts import render, redirect
+from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+# --------------------------------------------------------
+from ibs.models import ProjectAccountD3
+
+TODAY = date.today()
+
+
+def install_check(request):
+    is_d3 = ProjectAccountD3.objects.exists()
+
+    if is_d3:
+        return render(request, 'base-vue.html')
+    else:
+        return redirect('/install/')
+
+
+class Dashboard(LoginRequiredMixin, TemplateView):
+    template_name = 'ibs/main/1_1_dashboard.html'
+
+
+def menu2_1(request):
+    return render(request, 'ibs/main/2_1_schedule.html')
+
+
+class CustomHandler404(generic.View):
+    @staticmethod
+    def get(request):
+        context = {}
+        return render(request, "errors/404.html", context)
+
+
+def handler500(request):
+    context = {}
+    response = render(request, "errors/500.html", context=context)
+    response.status_code = 500
+    return response
