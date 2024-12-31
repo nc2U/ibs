b9b74c6fa75c8e833fc01a135e25e3339e513838
Austin Kho <kori.susie@gmail.com>
2025-01-01 02:22:44 +0900

    gantt chart update ing

--- app/vue/src/views/_Work/Manages/Gantt/components/GanttChart.vue
+++ app/vue/src/views/_Work/Manages/Gantt/components/GanttChart.vue
@@ -1,28 +1,32 @@
 <script lang="ts" setup>
 import { computed, type ComputedRef, inject, type PropType } from 'vue'
-import { getToday } from '@/utils/baseMixins'
-import type { Gantts } from '@/store/types/work'
 import { GGanttChart } from '@infectoone/vue-ganttastic'
-import sanitizeHtml from 'sanitize-html'
+import type { Gantts } from '@/store/types/work'
+import { getToday } from '@/utils/baseMixins'
 
 const props = defineProps({ gantts: { type: Array as PropType<Gantts[][]>, default: () => [] } })
 
 const isDark = inject<ComputedRef<boolean>>('isDark')
 
-const chartStart = (date = new Date()) => new Date(date.getFullYear(), date.getMonth(), 1)
-const chartEnd = (date = new Date()) => new Date(date.getFullYear(), date.getMonth() + 6, 0)
+const chartStart = (date = new Date()) => {
+  date.setDate(date.getDate() - 15)
+  return new Date(date.getFullYear(), date.getMonth(), 1)
+}
+const chartEnd = (date = new Date()) => {
+  date.setDate(date.getDate() - 15)
+  return new Date(date.getFullYear(), date.getMonth() + 6, 0)
+}
 
+const style = computed(() => `border-bottom: #${isDark?.value ? '666' : 'ddd'} 1px solid`)
 const remain = computed(() => (props.gantts.length > 10 ? 5 : 15 - props.gantts.length))
-
-const onMouseenterBar = (bar: any) => console.log(bar)
-const onMouseleaveBar = (bar: any) => console.log(bar)
 </script>
 
 <template>
   <g-gantt-chart
-    :style="`border-bottom: #${isDark ? '666' : 'ddd'} 1px solid`"
-    :current-time-label="getToday()"
+    :color-scheme="isDark ? 'dark' : ''"
+    label-column-width="450px"
     label-column-title=" "
+    :current-time-label="getToday()"
     :chart-start="chartStart()"
     :chart-end="chartEnd()"
     date-format="YYYY-MM-DD"
@@ -30,12 +34,9 @@ const onMouseleaveBar = (bar: any) => console.log(bar)
     bar-start="start"
     bar-end="due"
     :row-height="20"
-    label-column-width="450px"
-    :color-scheme="isDark ? 'dark' : ''"
     current-time
+    :style="style"
     grid
-    @mouseenter-bar="onMouseenterBar($event.bar)"
-    @mouseleave-bar="onMouseleaveBar($event.bar)"
   >
     <g-gantt-row label="" :bars="[]" />
     <g-gantt-row
