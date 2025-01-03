ef66ad320ad5c8056df2251caa18a25bc9e68fd5
Austin Kho <kori.susie@gmail.com>
2025-01-03 22:50:10 +0900

    ibs api update

--- app/django/apiV1/views/ibs.py
+++ app/django/apiV1/views/ibs.py
@@ -65,5 +65,5 @@ class ProjectAccountD3ViewSet(viewsets.ModelViewSet):
 class WiseSayViewSet(viewsets.ModelViewSet):
     queryset = WiseSaying.objects.all()
     serializer_class = WiseSaySerializer
-    pagination_class = PageNumberPaginationFifty
+    pagination_class = PageNumberPaginationThreeHundred
     permissions_classes = (permissions.IsAuthenticated, IsProjectStaffOrReadOnly)
