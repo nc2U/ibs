72757c68581de4d5feaef29dd8234a960030545b
Austin Kho <kori.susie@gmail.com>
2025-03-02 19:11:23 +0900

    work docs update

--- app/vue/src/views/_Work/Manages/Projects/components/Documents/components/DocsForm.vue
+++ app/vue/src/views/_Work/Manages/Projects/components/Documents/components/DocsForm.vue
@@ -1,5 +1,7 @@
 <script lang="ts" setup>
 import { ref } from 'vue'
+import { colorLight } from '@/utils/cssMixins'
+import MdEditor from '@/components/MdEditor/Index.vue'
 import AddNewDoc from './AddNewDoc.vue'
 
 defineProps({
@@ -14,5 +16,46 @@ defineProps({
     </CCol>
 
     <AddNewDoc :proj-status="projStatus" />
+  </CRow>
+
+  <CRow>
+    <CCard :color="colorLight" class="mb-3">
+      <CCardBody>
+        <CRow class="mb-3">
+          <CFormLabel class="col-form-label text-right col-2">범주</CFormLabel>
+          <CCol class="col-sm-10 col-md-6 col-lg-4 col-xl-3">
+            <CFormSelect></CFormSelect>
+          </CCol>
+        </CRow>
+
+        <CRow class="mb-3">
+          <CFormLabel class="col-form-label text-right col-2 required">제목</CFormLabel>
+          <CCol class="col-sm-10">
+            <CFormInput />
+          </CCol>
+        </CRow>
+
+        <CRow class="mb-3">
+          <CFormLabel class="col-form-label text-right col-2">설명</CFormLabel>
+          <CCol class="col-sm-10">
+            <MdEditor></MdEditor>
+          </CCol>
+        </CRow>
+
+        <CRow class="">
+          <CFormLabel class="col-form-label text-right col-2">파일</CFormLabel>
+          <CCol class="col-sm-10">
+            <CFormInput />
+          </CCol>
+        </CRow>
+      </CCardBody>
+    </CCard>
+  </CRow>
+
+  <CRow class="mb-5">
+    <CCol>
+      <CButton type="submit" color="primary" variant="outline"> 저장</CButton>
+      <CButton color="light" type="submit">취소</CButton>
+    </CCol>
   </CRow>
 </template>
