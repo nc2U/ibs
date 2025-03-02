431b80e2e7f271827bd5ebd6a4bff33087fbc768
Austin Kho <kori.susie@gmail.com>
2025-03-02 17:39:07 +0900

    work project app update

--- app/vue/src/views/_Work/Manages/Projects/components/Documents/Index.vue
+++ app/vue/src/views/_Work/Manages/Projects/components/Documents/Index.vue
@@ -20,7 +20,7 @@ onBeforeMount(() => emit('aside-visible', true))
     </CCol>
 
     <CCol class="text-right">
-      <span v-if="issueProject.status !== '9'" class="mr-2 form-text">
+      <span v-if="issueProject?.status !== '9'" class="mr-2 form-text">
         <v-icon icon="mdi-plus-circle" color="success" size="sm" />
         <router-link to="" class="ml-1">새 문서</router-link>
       </span>
