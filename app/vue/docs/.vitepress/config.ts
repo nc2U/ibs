b1b7ba1238b14322f18a9defc66f5dfab45370c4
Austin Kho <kori.susie@gmail.com>
2025-03-10 18:48:04 +0900

    docs update

--- app/vue/docs/.vitepress/config.ts
+++ app/vue/docs/.vitepress/config.ts
@@ -23,7 +23,7 @@ export default defineConfig({
   },
   themeConfig: {
     logo: 'https://raw.githubusercontent.com/nc2U/ibs/a39d44a6f22b37ce7444a5102fa7bb4d9b00db66/app/vue/docs/favicon.svg',
-    siteTitle: 'IBS',
+    siteTitle: 'DAEYOUNG IBS',
     nav: [
       { text: '가이드', link: '/intro/getting-started' },
       {
