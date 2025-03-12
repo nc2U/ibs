55bba74220156852bd7327116aeb6864dbf0ebc6
Austin Kho <kori.susie@gmail.com>
2025-03-12 22:35:20 +0900

    comDocs update

--- app/vue/src/views/comDocs/LawsuitDocs/Index.vue
+++ app/vue/src/views/comDocs/LawsuitDocs/Index.vue
@@ -169,7 +169,7 @@ const onSubmit = async (payload: Docs & Attatches) => {
     } else {
       await createDocs({ form })
       await router.replace({ name: `${mainViewName.value}` })
-      fController.value.resetForm()
+      // fController.value.resetForm(false)
     }
     newFiles.value = []
     cngFiles.value = []
