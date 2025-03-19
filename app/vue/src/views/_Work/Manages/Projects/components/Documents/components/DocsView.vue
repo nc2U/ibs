b6d5b209d1e49f7fa1c8f8a2cdbf88f1e6672215
Austin Kho <kori.susie@gmail.com>
2025-03-19 22:55:44 +0900

    work docs view update

--- app/vue/src/views/_Work/Manages/Projects/components/Documents/components/DocsView.vue
+++ app/vue/src/views/_Work/Manages/Projects/components/Documents/components/DocsView.vue
@@ -1,48 +1,85 @@
 <script lang="ts" setup>
-import { type PropType } from 'vue'
+import { computed, onBeforeMount, onMounted, type PropType } from 'vue'
 import type { Docs } from '@/store/types/docs'
-import { elapsedTime, timeFormat } from '../../../../../../../utils/baseMixins'
+import { useDocs } from '@/store/pinia/docs'
+import { useRoute, useRouter } from 'vue-router'
+import { timeFormat } from '@/utils/baseMixins'
+import sanitizeHtml from 'sanitize-html'
 
-defineProps({ docs: { type: Object as PropType<Docs>, required: true } })
+const props = defineProps({
+  docs: { type: Object as PropType<Docs>, required: true },
+  heatedPage: { type: Array as PropType<number[]>, default: () => [] },
+})
+
+const emit = defineEmits(['docs-hit'])
+
+const docStore = useDocs()
+const [route, router] = [useRoute(), useRouter()]
+
+const docId = computed(() => Number(route.params.docId))
+
+onBeforeMount(() => {
+  if (docId.value) {
+    docStore.fetchDocs(docId.value)
+  }
+})
+
+onMounted(() => {
+  if (docId.value && !props.heatedPage?.includes(docId.value)) {
+    emit('docs-hit', docId.value)
+  }
+})
 </script>
 
 <template>
-  <CRow class="py-2">
-    <CCol>
-      <h5>{{ docs.title }}</h5>
-    </CCol>
+  <div v-if="docs">
+    <CRow class="py-2">
+      <CCol>
+        <h5>{{ docs.title }}</h5>
+      </CCol>
 
-    <CCol class="text-right">
-      <span class="mr-2 form-text">
-        <v-icon icon="mdi-pencil" color="warning" size="sm" />
-        <router-link :to="{ name: '(문서) - 편집' }" class="ml-1">편집</router-link>
-      </span>
+      <CCol class="text-right">
+        <span class="mr-2 form-text">
+          <v-icon icon="mdi-pencil" color="warning" size="sm" />
+          <router-link :to="{ name: '(문서) - 편집' }" class="ml-1">편집</router-link>
+        </span>
 
-      <span class="mr-2 form-text">
-        <v-icon icon="mdi-trash-can-outline" color="secondary" size="sm" />
-        <router-link :to="{ name: '(문서) - 삭제' }" class="ml-1">삭제</router-link>
-      </span>
-    </CCol>
-  </CRow>
+        <span class="mr-2 form-text">
+          <v-icon icon="mdi-trash-can-outline" color="secondary" size="sm" />
+          <router-link :to="{ name: '(문서) - 삭제' }" class="ml-1">삭제</router-link>
+        </span>
+      </CCol>
+    </CRow>
 
-  <CRow class="mb-5">
-    <CCol>
-      {{ docs.proj_name }} / {{ docs.cate_name }} ({{
-        timeFormat(docs.created as string, true, '/')
-      }})
-    </CCol>
-    <CCol></CCol>
-  </CRow>
+    <CRow class="mb-5">
+      <CCol>
+        {{ docs.proj_name }} / {{ docs.cate_name }} ({{
+          timeFormat(docs.created as string, true, '/')
+        }})
+      </CCol>
+      <CCol></CCol>
+    </CRow>
 
-  <CRow>
-    <CCol> {{ docs }}</CCol>
-  </CRow>
+    <CRow class="mb-5">
+      <CCol>
+        <div v-html="sanitizeHtml(docs.content)" />
+      </CCol>
+    </CRow>
 
-  <CRow class="mt-5">
-    <CCol class="text-right">
-      <CButton color="light" @click="$router.replace({ name: '(문서)' })" size="sm">
-        목록으로
-      </CButton>
-    </CCol>
-  </CRow>
+    <CRow>
+      <CCol><h5>파일</h5></CCol>
+    </CRow>
+
+    <CRow>
+      <CCol>ㅁㄴㅇㅁㄴㅇㄹㅁㄴㅇㄹ</CCol>
+    </CRow>
+
+    <CRow class="mt-5">
+      <CCol class="text-right">
+        <CButton color="light" @click="router.replace({ name: '(문서)' })" size="sm">
+          목록으로
+        </CButton>
+      </CCol>
+    </CRow>
+  </div>
 </template>
