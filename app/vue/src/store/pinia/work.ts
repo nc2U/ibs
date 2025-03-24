72038cc009d99c54384849839f303e564c778d85
Austin Kho <kori.susie@gmail.com>
2025-03-25 01:21:54 +0900

    work issue allProjects debug

--- app/vue/src/store/pinia/work.ts
+++ app/vue/src/store/pinia/work.ts
@@ -81,7 +81,7 @@ export const useWork = defineStore('work', () => {
     com: '' | number = '',
     sort: '' | '1' | '2' | '3' = '2',
     p_isnull: '' | '1' = '1',
-    status: '1' | '9' = '1',
+    status: '' | '1' | '9' = '1',
   ) =>
     await api
       .get(
