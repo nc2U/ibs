ef808eef8d109fcf659edd241eeabc412eeb6ee4
austin <kori.susie@gmail.com>
2025-04-14 11:21:22 +0900

    project model update

--- app/django/project/models.py
+++ app/django/project/models.py
@@ -122,7 +122,7 @@ class Site(models.Model):
         return f'{self.district} {self.lot_number}'
 
     class Meta:
-        ordering = ('-project', '-order', 'lot_number')
+        ordering = ('-project', 'order', 'lot_number')
         verbose_name = '04. 사업부지 목록'
         verbose_name_plural = '04. 사업부지 목록'
 
