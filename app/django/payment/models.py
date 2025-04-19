bfcce50aa7f27cf4645f9653d932668f9825cc93
Austin Kho <kori.susie@gmail.com>
2025-04-19 21:55:46 +0900

    payment model update

--- app/django/payment/models.py
+++ app/django/payment/models.py
@@ -31,15 +31,17 @@ class InstallmentPaymentOrder(models.Model):  # 분할 납부 차수 등록
     pay_ratio = models.DecimalField('회당 납부비율(%)', max_digits=7, decimal_places=4, null=True, blank=True,
                                     help_text='''분양가 대비 납부비율, 계약금 항목인 경우 Downpayment 
                                     테이블 데이터 우선, 잔금 항목인 경우 분양가와 비교 차액 데이터 우선''')
+    pay_amt = models.PositiveIntegerField('납부 약정금액', null=True, blank=True,
+                                          help_text='약정금이 차수, 타입에 관계 없이 정액인 경우 설정(예: 세대별 업무대행비)')
     is_pm_cost = models.BooleanField('PM용역비 여부', default=False)
     is_calc_start = models.BooleanField('할인/가산 시작 여부', default=False)
     pay_name = models.CharField('납부회차 명', max_length=20)
     alias_name = models.CharField('회차 별칭', max_length=20, blank=True)
     days_since_prev = models.PositiveSmallIntegerField('전회 기준 경과일수', null=True, blank=True,
                                                        help_text="전 회차(예: 계약일)로부터 __일 이내 형식으로 납부기한을 지정할 경우 해당 일수")
-    pay_due_date = models.DateField('지정 납부기한', null=True, blank=True, help_text="특정일자를 납부기한으로 지정할 경우")
-    extra_due_date = models.DateField('납부유예일', null=True, blank=True,
-                                      help_text='연체료 계산 기준은 지정 납부기한이 원칙이나 이 값이 있는 경우 납부유예일을 연체료 계산 기준으로 한다.')
+    pay_due_date = models.DateField('냡부 약정일', null=True, blank=True, help_text="특정일자를 납부기한으로 지정할 경우")
+    extra_due_date = models.DateField('연체 기준일', null=True, blank=True,
+                                      help_text='연체료 계산 기준은 납부 약정일이 원칙이나 이 값이 있는 경우 연체 기준일을 연체료 계산 기준으로 한다.')
 
     def __str__(self):
         return f'[{self.get_pay_sort_display()}] - {self.pay_name}'
