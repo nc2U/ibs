9467f847e792d4ed137cc6ca3dc3b6127643c202
austin <kori.susie@gmail.com>
2025-05-02 13:52:50 +0900

    backup script update

--- volume/backups/dump_postgres.sh
+++ volume/backups/dump_postgres.sh
@@ -8,7 +8,7 @@ SQL_FILE=/var/backups/backup-postgres-${DATE}.dump
 find /var/backups -name "*.dump" -mtime +2 -type f -delete
 
 # (3) do the postgres database backup (dump)
-PGPASSWORD="${POSTGRES_PASSWORD}" pg_dump -U "${POSTGRES_USER}" -d "${POSTGRES_DB}" --exclude-table=django_migrations -Fc -f "${SQL_FILE}"
+pg_dump -U "${POSTGRES_USER}" -d "${POSTGRES_DB}" --exclude-table=django_migrations -Fc -f "${SQL_FILE}"
 
 # 백업이 성공했는지 확인
 if [ $? -eq 0 ]; then
