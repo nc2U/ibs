343d65cc37fb179b4e3541fb7e640267c2fb4110
Austin Kho <kori.susie@gmail.com>
2025-05-02 22:13:37 +0900

    issue project list update

--- app/vue/src/views/_Work/Manages/Projects/components/ProjectList.vue
+++ app/vue/src/views/_Work/Manages/Projects/components/ProjectList.vue
@@ -1,9 +1,15 @@
 <script lang="ts" setup>
-import { inject, onBeforeMount, type ComputedRef, type PropType } from 'vue'
-import type { User } from '@/store/types/accounts'
+import {
+  type ComputedRef,
+  inject,
+  onBeforeMount,
+  onBeforeUnmount,
+  onMounted,
+  type PropType,
+  ref,
+} from 'vue'
 import { useRoute } from 'vue-router'
 import type { IssueProject, ProjectFilter } from '@/store/types/work'
-import { VueMarkdownIt } from '@f3ve/vue-markdown-it'
 import SearchList from './SearchList.vue'
 import ProjectCard from './ProjectCard.vue'
 import NoData from '@/views/_Work/components/NoData.vue'
@@ -21,6 +27,21 @@ const route = useRoute()
 
 const filterSubmit = (payload: ProjectFilter) => emit('filter-submit', payload)
 
+const breakpoint = ref('xl')
+
+const updateBreakpoint = () => {
+  const width = window.innerWidth
+  if (width < 768) breakpoint.value = 'sm'
+  else if (width < 992) breakpoint.value = 'md'
+  else if (width < 1200) breakpoint.value = 'lg'
+  else breakpoint.value = 'xl'
+}
+
+onMounted(() => {
+  updateBreakpoint()
+  window.addEventListener('resize', updateBreakpoint)
+})
+onBeforeUnmount(() => window.removeEventListener('resize', updateBreakpoint))
 onBeforeMount(() => emit('aside-visible', true))
 </script>
 
@@ -47,9 +68,63 @@ onBeforeMount(() => emit('aside-visible', true))
   <NoData v-if="!projectList.length" />
 
   <CRow v-else>
-    <template v-for="(proj, i) in projectList" :key="proj.pk">
-      <CCol sm="6">
+    <CCol v-if="breakpoint === 'sm'">
+      <template v-for="proj in projectList" :key="proj.pk">
         <ProjectCard :project="proj" />
+      </template>
+    </CCol>
+
+    <template v-else-if="breakpoint === 'md'">
+      <CCol sm="6">
+        <template v-for="(proj, i) in projectList" :key="proj.pk">
+          <ProjectCard v-if="(i + 1) % 2 == 1" :project="proj" />
+        </template>
+      </CCol>
+      <CCol sm="6">
+        <template v-for="(proj, i) in projectList" :key="proj.pk">
+          <ProjectCard v-if="(i + 1) % 2 == 0" :project="proj" />
+        </template>
+      </CCol>
+    </template>
+
+    <template v-else-if="breakpoint === 'lg'">
+      <CCol sm="4">
+        <template v-for="(proj, i) in projectList" :key="proj.pk">
+          <ProjectCard v-if="(i + 1) % 3 == 1" :project="proj" />
+        </template>
+      </CCol>
+      <CCol sm="4">
+        <template v-for="(proj, i) in projectList" :key="proj.pk">
+          <ProjectCard v-if="(i + 1) % 3 == 2" :project="proj" />
+        </template>
+      </CCol>
+      <CCol sm="4">
+        <template v-for="(proj, i) in projectList" :key="proj.pk">
+          <ProjectCard v-if="(i + 1) % 3 == 0" :project="proj" />
+        </template>
+      </CCol>
+    </template>
+
+    <template v-else>
+      <CCol sm="3">
+        <template v-for="(proj, i) in projectList" :key="proj.pk">
+          <ProjectCard v-if="(i + 1) % 4 == 1" :project="proj" />
+        </template>
+      </CCol>
+      <CCol sm="3">
+        <template v-for="(proj, i) in projectList" :key="proj.pk">
+          <ProjectCard v-if="(i + 1) % 4 == 2" :project="proj" />
+        </template>
+      </CCol>
+      <CCol sm="3">
+        <template v-for="(proj, i) in projectList" :key="proj.pk">
+          <ProjectCard v-if="(i + 1) % 4 == 3" :project="proj" />
+        </template>
+      </CCol>
+      <CCol sm="3">
+        <template v-for="(proj, i) in projectList" :key="proj.pk">
+          <ProjectCard v-if="(i + 1) % 4 == 0" :project="proj" />
+        </template>
       </CCol>
     </template>
   </CRow>
