b264dec3f3b04bcd09c6210be756e3fd3bab8a63
austin <kori.susie@gmail.com>
2025-05-02 14:19:50 +0900

    backup script update

--- volume/backups/restore_postgres.sh
+++ volume/backups/restore_postgres.sh
@@ -2,8 +2,7 @@
 DATE=$(date +"%Y-%m-%d")
 SQL_FILE="/var/backups/backup-postgres-${DATE}.dump"
 
-psql -U ${USER} -d ${DATABASE} -c "DROP SCHEMA ibs CASCADE; CREATE SCHEMA ibs;"
-pg_restore --clean --if-exists --no-owner -U "${POSTGRES_USER}" -d "${POSTGRES_DB}" "${SQL_FILE}"
+pg_restore --clean --if-exists -U "${POSTGRES_USER}" -d "${POSTGRES_DB}" "${SQL_FILE}"
 
 # 복원 성공 여부 확인
 if [ $? -eq 0 ]; then
