7d1626aaeca4cc2be9dfd68570fe86e91e38d8d8
austin <kori.susie@gmail.com>
2025-05-02 11:23:05 +0900

    backup script update

--- volume/backups/restore_postgres.sh
+++ volume/backups/restore_postgres.sh
@@ -2,7 +2,7 @@
 DATE=$(date +"%Y-%m-%d")
 SQL_FILE="/var/backups/backup-postgres-${DATE}.sql"
 
-PGPASSWORD="${POSTGRES_PASSWORD}" psql -U "${POSTGRES_USER}" -d "${POSTGRES_DB}" -f "${SQL_FILE}" 2> restore_error.log
+pg_restore  -U "${POSTGRES_USER}" -d "${POSTGRES_DB}" "${SQL_FILE}" 2> restore_error.log
 
 # 복원 성공 여부 확인
 if [ $? -eq 0 ]; then
