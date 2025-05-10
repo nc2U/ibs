6a65f673de64be6baef9e5576faa160f9e35f339
Austin Kho <kori.susie@gmail.com>
2025-05-10 16:19:57 +0900

    backup script update

--- volume/backups/restore_postgres.sh
+++ volume/backups/restore_postgres.sh
@@ -15,6 +15,18 @@ else
     echo "POSTGRES Database restoration failed"
 fi
 
+# 외래 키 제약 조건 비활성화
+psql -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c "
+DO \$\$
+DECLARE
+    r RECORD;
+BEGIN
+    FOR r IN (SELECT constraint_name, table_name FROM information_schema.table_constraints WHERE constraint_type = 'FOREIGN KEY' AND table_schema = 'ibs')
+    LOOP
+        EXECUTE 'ALTER TABLE ibs.' || quote_ident(r.table_name) || ' DROP CONSTRAINT ' || quote_ident(r.constraint_name);
+    END LOOP;
+END \$\$;
+"
 
 # 서버 B의 ibs 스키마에서 django_migrations를 제외한 테이블 데이터 삭제
 psql -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c "
@@ -36,7 +48,7 @@ if [ $? -ne 0 ]; then
 fi
 
 # 백업 파일 복원
-pg_restore -U "$POSTGRES_USER" -d "$POSTGRES_DB" --data-only --no-owner --no-privileges "$DUMP_FILE"
+pg_restore -U "$POSTGRES_USER" -d "$POSTGRES_DB" --data-only --no-owner --no-privileges --disable-triggers --jobs=4 "$DUMP_FILE"
 
 # 복원 결과 확인
 if [ $? -eq 0 ]; then
