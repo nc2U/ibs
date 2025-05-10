4d055711f35268eda23f67833529729a76118df0
Austin Kho <kori.susie@gmail.com>
2025-05-10 16:06:28 +0900

    cash model update

--- app/django/cash/models.py
+++ app/django/cash/models.py
@@ -52,7 +52,7 @@ class CashBook(models.Model):
     separated = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='sepItems',
                                   verbose_name='분할 계정')
     content = models.CharField('적요', max_length=50, blank=True, default='')
-    trader = models.CharField('거래처', max_length=20, blank=True, default='')
+    trader = models.CharField('거래처', max_length=25, blank=True, default='')
     bank_account = models.ForeignKey(CompanyBankAccount, on_delete=models.PROTECT, verbose_name='거래계좌')
     income = models.PositiveBigIntegerField('입금액', null=True, blank=True)
     outlay = models.PositiveBigIntegerField('출금액', null=True, blank=True)
@@ -120,7 +120,7 @@ class ProjectCashBook(models.Model):
                                           blank=True, verbose_name='환불 계약자',
                                           help_text='이 건 거래가 환불금 출금인 경우 이 건을 납부한 계약자를 선택')  # 환불 종결 여부
     content = models.CharField('적요', max_length=50, blank=True, default='')
-    trader = models.CharField('거래처', max_length=20, blank=True, default='',
+    trader = models.CharField('거래처', max_length=25, blank=True, default='',
                               help_text='분양대금(분담금) 수납 건인 경우 반드시 해당 계좌에 기재된 입금자를 기재')  # icp=True -> 분양대금 납입자
     bank_account = models.ForeignKey(ProjectBankAccount, on_delete=models.PROTECT,
                                      verbose_name='거래계좌')  # icp=True -> 분양대금 납입계좌
