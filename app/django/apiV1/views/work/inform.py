1a49fbcdafed2254106f36a449a3a5f48c57047a
Austin Kho <kori.susie@gmail.com>
2025-05-23 23:25:00 +0900

    #127 work api module update

--- app/django/apiV1/views/work/inform.py
+++ app/django/apiV1/views/work/inform.py
@@ -1,6 +1,4 @@
-from django_filters.rest_framework import FilterSet, BooleanFilter, DateFilter, CharFilter
 from rest_framework import viewsets
-from rest_framework.views import APIView
 
 from apiV1.pagination import *
 from apiV1.permission import *
