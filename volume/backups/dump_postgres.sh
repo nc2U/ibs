790f950d86845d52069618b1554d345ada02528d
Austin Kho <kori.susie@gmail.com>
2025-06-07 01:43:16 +0900

    backup script update

--- volume/backups/dump_postgres.sh
+++ volume/backups/dump_postgres.sh
@@ -5,7 +5,7 @@ DATE=$(date +"%Y-%m-%d")
 DUMP_FILE=/var/backups/backup-postgres-${DATE}.dump
 
 # 이전 백업 삭제 (예: 2일 이상된 파일)
-find /var/backups \(-name "*.dump" -o -name "*.log" \) -mtime +2 -type f -delete
+find /var/backups \( -name "*.dump" -o -name "*.log" \) -type f -ctime +2 -delete
 
 # pg_dump로 ibs 스키마의 데이터만 추출 (django_migrations 제외)
 pg_dump -U "${POSTGRES_USER}" -d "${POSTGRES_DB}" -n "${POSTGRES_USER}" \
