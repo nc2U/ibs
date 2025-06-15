704e4dabfe8679df2c187eaa8ed8a7acb97b3c7f
Austin Kho <kori.susie@gmail.com>
2025-06-16 01:31:52 +0900

    #128 change-set activities update ing

--- app/django/apiV1/views/work/logging.py
+++ app/django/apiV1/views/work/logging.py
@@ -4,8 +4,9 @@ from rest_framework import viewsets
 
 from apiV1.pagination import *
 from apiV1.permission import *
+from apiV1.serializers.work import CommitSerializer
 from apiV1.serializers.work.logging import *
-from work.models import IssueProject
+from work.models import IssueProject, Commit
 
 
 class ActivityLogFilter(FilterSet):
@@ -56,17 +57,74 @@ class ActivityLogEntryViewSet(viewsets.ModelViewSet):
     queryset = ActivityLogEntry.objects.all()
     serializer_class = ActivityLogEntrySerializer
     permission_classes = (permissions.IsAuthenticated,)
-    pagination_class = PageNumberPaginationThreeHundred
+    # pagination_class = PageNumberPaginationThreeHundred
+    pagination_class = PageNumberPaginationOneHundred
     filterset_class = ActivityLogFilter
 
     def get_queryset(self):
+        """ActivityLogEntry 필터링: 사용자 권한 기반"""
         user = self.request.user
         work_auth = user.work_manager or user.is_superuser
         projects = user.assigned_projects()
         return self.queryset \
             if work_auth \
-            else self.queryset.filter(Q(project__is_public=True) |
-                                      Q(project__in=projects))
+            else self.queryset.filter(
+            Q(project__is_public=True) |
+            Q(project__in=projects)).select_related(
+            'project', 'issue', 'comment', 'user', 'spent_time')
+
+    # def get_commits(self):
+    #     """Commit 데이터 조회: 프로젝트 권한 적용"""
+    #     user = self.request.user
+    #     work_auth = user.work_manager or user.is_superuser
+    #     projects = user.assigned_projects()
+    #     queryset = Commit.objects.filter(repo__slug='ibs')
+    #     if not work_auth:
+    #         queryset = queryset.filter(
+    #             Q(issues__project__is_public=True) |
+    #             Q(issues__project__in=projects)
+    #         )
+    #     return queryset.select_related('repo').prefetch_related('issues').distinct().order_by('-date')
+    #
+    # def list(self, request, *args, **kwargs):
+    #     """ActivityLogEntry와 Commit 데이터를 병합하여 반환"""
+    #     # ActivityLogEntry 조회
+    #     queryset = self.filter_queryset(self.get_queryset())
+    #     logs = queryset.order_by('-timestamp')
+    #     log_serializer = self.get_serializer(logs, many=True)
+    #     log_data = log_serializer.data
+    #
+    #     # Commit 조회
+    #     commits = self.get_commits()
+    #     commit_serializer = CommitSerializer(commits, many=True)
+    #     commit_data = commit_serializer.data
+    #
+    #     # 데이터 병합
+    #     combined = [
+    #         *[{"data": log} for log in log_data],
+    #         *[{
+    #             "data": {
+    #                 "sort": "3",
+    #                 "commit_hash": c["commit_hash"],
+    #                 "timestamp": c["date"],
+    #                 "status_log": c["message"][:30],
+    #                 "user": {"username": c["author"]} if c["author"] else None,  # User 매핑
+    #                 "project": c["issues"][0]["project"] if c["issues"] else None
+    #             }
+    #         } for c in commit_data]
+    #     ]
+    #
+    #     # 시간순 정렬
+    #     combined = sorted(
+    #         combined,
+    #         key=lambda x: x["data"]["timestamp"],
+    #         reverse=True
+    #     )
+    #
+    #     # 페이지네이션 적용
+    #     paginator = self.pagination_class()
+    #     page = paginator.paginate_queryset(combined, request)
+    #     return paginator.get_paginated_response(page)
 
 
 class IssueLogEntryViewSet(viewsets.ModelViewSet):
