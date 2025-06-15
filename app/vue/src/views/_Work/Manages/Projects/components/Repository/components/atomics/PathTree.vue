de9dffa098c3cd4f7608d74e520197f1b07e1712
Austin Kho <kori.susie@gmail.com>
2025-06-15 21:00:04 +0900

    #127 git path tree update

--- app/vue/src/views/_Work/Manages/Projects/components/Repository/components/atomics/PathTree.vue
+++ app/vue/src/views/_Work/Manages/Projects/components/Repository/components/atomics/PathTree.vue
@@ -56,31 +56,38 @@ const choiceFunc = (isFile: boolean, path: string, index: number) => {
     </CCol>
   </CRow>
 
-  <CRow v-for="(tree, i) in changeFiles" :key="i">
+  <CRow v-for="(file, i) in changeFiles" :key="i">
     <CCol>
-      <CRow v-for="(item, j) in pathList(tree.path)" :key="j" class="pl-5">
-        <CCol :style="`padding-left: ${j * 25}px`">
-          <span v-if="j !== pathList(tree.path).length - 1" class="mr-2">
-            <v-icon icon="mdi-folder-open" color="#EFD2A8" size="16" />
-          </span>
-          <span v-else class="mr-2" style="font-size: 0.8em">
-            <v-icon v-if="tree.type === 'A'" color="success" icon="mdi-plus-circle" size="" />
-            <v-icon v-else-if="tree.type === 'D'" color="danger" icon="mdi-minus-circle" size="" />
-            <v-icon v-else-if="tree.type === 'R'" color="purple" icon="mdi-circle" size="" />
-            <v-icon v-else-if="tree.type === 'C'" color="info" icon="mdi-circle" size="" />
-            <v-icon v-else color="warning" icon="mdi-circle" size="" />
-          </span>
-          <span>
-            <router-link
-              to=""
-              @click="choiceFunc(pathList(tree.path).length - 1 === j, tree.path, j)"
-            >
-              {{ item }}
-            </router-link>
-          </span>
-          <span v-if="j === pathList(tree.path).length - 1">
-            (<router-link to="" @click="emit('diff-view', i)">비교(diff)</router-link>)
-          </span>
+      <CRow v-for="(item, j) in pathList(file.path)" :key="j" class="pl-5">
+        <CCol :style="`padding-left: ${j * 20}px`">
+          <CCol v-if="i === 0 || pathList(changeFiles[i - 1].path)[j] !== item">
+            <span v-if="j !== pathList(file.path).length - 1" class="mr-2">
+              <v-icon icon="mdi-folder-open" color="#EFD2A8" size="16" />
+            </span>
+            <span v-else class="mr-2" style="font-size: 0.8em">
+              <v-icon v-if="file.type === 'A'" color="success" icon="mdi-plus-circle" size="" />
+              <v-icon
+                v-else-if="file.type === 'D'"
+                color="danger"
+                icon="mdi-minus-circle"
+                size=""
+              />
+              <v-icon v-else-if="file.type === 'R'" color="purple" icon="mdi-circle" size="" />
+              <v-icon v-else-if="file.type === 'C'" color="info" icon="mdi-circle" size="" />
+              <v-icon v-else color="warning" icon="mdi-circle" size="" />
+            </span>
+            <span>
+              <router-link
+                to=""
+                @click="choiceFunc(pathList(file.path).length - 1 === j, file.path, j)"
+              >
+                {{ item }}
+              </router-link>
+            </span>
+            <span v-if="j === pathList(file.path).length - 1">
+              (<router-link to="" @click="emit('diff-view', i)">비교(diff)</router-link>)
+            </span>
+          </CCol>
         </CCol>
       </CRow>
     </CCol>
