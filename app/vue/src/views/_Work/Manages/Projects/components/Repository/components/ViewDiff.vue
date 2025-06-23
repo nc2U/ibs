768d7c1ab491a45410a05c35e29fba0b5f0a442b
Austin Kho <kori.susie@gmail.com>
2025-06-23 10:10:45 +0900

    #127 repo app update

--- app/vue/src/views/_Work/Manages/Projects/components/Repository/components/ViewDiff.vue
+++ app/vue/src/views/_Work/Manages/Projects/components/Repository/components/ViewDiff.vue
@@ -1,10 +1,11 @@
 <script lang="ts" setup>
-import { computed, onBeforeMount, onBeforeUnmount, type PropType } from 'vue'
+import { computed, onBeforeMount, onBeforeUnmount, type PropType, ref } from 'vue'
 import { btnSecondary } from '@/utils/cssMixins.ts'
 import { useRoute, useRouter } from 'vue-router'
 import { useGitRepo } from '@/store/pinia/work_git_repo.ts'
 import type { DiffApi } from '@/store/types/work_git_repo.ts'
 import 'diff2html/bundles/css/diff2html.min.css'
+import Loading from '@/components/Loading/Index.vue'
 import Diff from './atomics/Diff.vue'
 
 const [route, router] = [useRoute(), useRouter()]
@@ -16,9 +17,9 @@ const repo = computed(() => Number(route.params.repoId))
 const base = computed(() => route.params.base as string)
 const head = computed(() => route.params.head as string)
 
-const getDiff = () => {
+const getDiff = async () => {
   const diff_hash = `?base=${base.value}&head=${head.value}`
-  gitStore.fetchGitDiff(repo.value as number, diff_hash)
+  await gitStore.fetchGitDiff(repo.value as number, diff_hash)
 }
 
 const getBack = () => {
@@ -26,11 +27,16 @@ const getBack = () => {
   gitStore.removeGitDiff()
 }
 
-onBeforeMount(getDiff)
+const loading = ref<boolean>(true)
+onBeforeMount(async () => {
+  await getDiff()
+  loading.value = false
+})
 onBeforeUnmount(gitStore.removeGitDiff)
 </script>
 
 <template>
+  <Loading v-model:active="loading" />
   <CRow class="py-2">
     <CCol>
       <h5>리비전 {{ base.substring(0, 8) }} : {{ head.substring(0, 8) }}</h5>
