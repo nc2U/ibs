99e2daf1b55051ef1baacda233e6a7b0a755e40e
Austin Kho <kori.susie@gmail.com>
2025-06-27 15:50:34 +0900

    #127 git api update

--- app/django/apiV1/views/work/git_repo.py
+++ app/django/apiV1/views/work/git_repo.py
@@ -73,29 +73,30 @@ class CommitViewSet(viewsets.ModelViewSet):
     def calculate_space(commits):
         space_map = {}
         current_space = 0
-        active_branches = {}
+        # active_branches = {}
+
         # 페이지네이션된 커밋만 처리, 최신순 정렬
         for commit in sorted(commits, key=lambda c: c.date, reverse=True):
             commit_hash = commit.commit_hash
             parents = list(commit.parents.values_list('commit_hash', flat=True))
-            if not parents:
-                space_map[commit_hash] = current_space
-                active_branches[commit_hash] = current_space
-                current_space += 1
-            else:
-                if len(parents) > 1:  # Merge commit
-                    spaces = [space_map.get(ph) for ph in parents if ph in space_map]
-                    space = min(spaces) if spaces else current_space
-                    space_map[commit_hash] = space
-                    for ph in parents:
-                        active_branches.pop(ph, None)
-                    active_branches[commit_hash] = space
-                else:
+            if parents:
+                if len(parents) == 1:  # 단일 커밋
                     parent_hash = parents[0]
                     space = space_map.get(parent_hash, current_space)
                     space_map[commit_hash] = space
-                    active_branches[commit_hash] = space
-                    active_branches.pop(parent_hash, None)
+                    # active_branches[commit_hash] = space
+                    # active_branches.pop(parent_hash, None)
+                else:  # Merge commit
+                    spaces = [space_map.get(ph) for ph in parents if ph in space_map]
+                    space = min(spaces) if spaces else current_space
+                    space_map[commit_hash] = space
+                    # for ph in parents:
+                    #     active_branches.pop(ph, None)
+                    # active_branches[commit_hash] = space
+            else:
+                space_map[commit_hash] = current_space
+                # active_branches[commit_hash] = current_space
+                current_space += 1
         return space_map
 
     @action(detail=False, methods=['get'], url_path='graph')
