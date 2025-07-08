7c17945a91d381ebd72ba19c26855a27742f3e0a
Austin Kho <kori.susie@gmail.com>
2025-07-08 22:59:40 +0900

    #75 board model update

--- app/vue/src/views/_Work/Manages/Projects/components/Settings/components/Forum.vue
+++ app/vue/src/views/_Work/Manages/Projects/components/Settings/components/Forum.vue
@@ -1,8 +1,12 @@
 <script lang="ts" setup>
-import { computed, onBeforeMount, watch } from 'vue'
+import { computed, onBeforeMount, ref, watch } from 'vue'
 import { useRoute } from 'vue-router'
 import { useBoard } from '@/store/pinia/board.ts'
+import draggable from 'vuedraggable'
 import NoData from '@/views/_Work/components/NoData.vue'
+import FormModal from '@/components/Modals/FormModal.vue'
+
+const RefForumForm = ref()
 
 const route = useRoute()
 
@@ -16,9 +20,7 @@ watch(projId, nVal => {
   if (nVal) fetchBoardList({ project: nVal })
 })
 
-onBeforeMount(() => {
-  fetchBoardList({ project: projId.value })
-})
+onBeforeMount(() => fetchBoardList({ project: projId.value }))
 </script>
 
 <template>
@@ -26,7 +28,7 @@ onBeforeMount(() => {
     <CCol>
       <span class="mr-2 form-text">
         <v-icon icon="mdi-plus-circle" color="success" size="15" />
-        <router-link to="" class="ml-1">새 게시판</router-link>
+        <router-link to="" class="ml-1" @click="RefForumForm.callModal()">새 게시판</router-link>
       </span>
     </CCol>
   </CRow>
@@ -36,28 +38,42 @@ onBeforeMount(() => {
   <CRow v-else>
     <CCol class="mt-3">
       <CTable table small hover responsive>
-        <col width="40" />
-        <col width="40" />
-        <col width="30" />
+        <col style="width: 30%" />
+        <col style="width: 45%" />
+        <col style="width: 25%" />
         <CTableHead>
           <CTableRow color="secondary">
-            <CTableHeaderCell colspan="3">게시판</CTableHeaderCell>
+            <CTableHeaderCell class="pl-2" colspan="3">게시판</CTableHeaderCell>
           </CTableRow>
         </CTableHead>
 
         <CTableBody>
           <CTableRow v-for="brd in boardList" :key="brd.pk">
-            <CTableDataCell>
+            <CTableDataCell class="pl-2">
               <router-link to="">{{ brd.name }}</router-link>
             </CTableDataCell>
             <CTableDataCell>{{ brd.description }}</CTableDataCell>
             <CTableDataCell>
-              <v-icon icon="mdi-pencil" color="amber" size="15" />
-              <v-icon icon="mdi-trash-can-outline" color="grey" size="15" />
+              <v-icon
+                icon="mdi-arrow-up-down-bold"
+                color="success"
+                size="16"
+                class="cursor-move mr-3"
+              />
+              <span class="mr-3 cursor-pointer">
+                <v-icon icon="mdi-pencil" color="amber" size="15" class="mr-2" />
+                <router-link to="">편집</router-link>
+              </span>
+              <span>
+                <v-icon icon="mdi-trash-can-outline" color="grey" size="15" class="mr-2" />
+                <router-link to="">삭제</router-link>
+              </span>
             </CTableDataCell>
           </CTableRow>
         </CTableBody>
       </CTable>
     </CCol>
   </CRow>
+
+  <FormModal ref="RefForumForm" size="lg"></FormModal>
 </template>
