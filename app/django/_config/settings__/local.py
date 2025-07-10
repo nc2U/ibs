a537d6ae83c92b70c422940d3946c8b386fae258
Austin Kho <kori.susie@gmail.com>
2025-07-10 23:17:56 +0900

    settings module update

--- app/django/_config/settings__/local.py
+++
@@ -1,8 +0,0 @@
-from .base import *
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ['*']
-
-CSRF_TRUSTED_ORIGINS = ['http://localhost', 'http://localhost:8000', 'http://localhost:8888', 'http://localhost:5173']
