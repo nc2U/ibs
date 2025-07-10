ac281c8f79eda9387d2dc54539ad8463a916da71
Austin Kho <kori.susie@gmail.com>
2025-07-10 23:01:17 +0900

    settings module update

--- app/django/_config/settings__/base.py
+++ app/django/_config/settings__/base.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('DJANGO_SECRET_KEY', default='SECRET_KEY')
+SECRET_KEY = config('SECRET_KEY', default='SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='', cast=Csv())
 
 SITE_ID = 1
 
