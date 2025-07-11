8f635b9f01e6b2617852eddd75c784d69bcdb0d4
Austin Kho <kori.susie@gmail.com>
2025-07-11 21:26:37 +0900

    #133 helm chart update

--- volume/initdb/postgres/init-schema.sql
+++
@@ -1,8 +0,0 @@
--- 1. ibs 스키마가 없으면 생성
-CREATE SCHEMA IF NOT EXISTS ibs;
-
--- 2. ibs 유저에게 ibs 스키마 권한 부여
-GRANT ALL ON SCHEMA ibs TO ibs;
-
--- 3. 해당 데이터베이스의 기본 검색 경로를 ibs → public 순서로 설정
-ALTER DATABASE ibs SET search_path TO ibs, public;
