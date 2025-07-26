b721f63f2064742b16d50f91f1bcb8eb93d6d597
Austin Kho <kori.susie@gmail.com>
2025-07-26 17:25:36 +0900

    .gitignore update

---
+++ deploy/helm/dev-deploy.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# .env 스크립트가 있는 디렉터리 경로 계산
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/../../app/django"
+
+# .env 파일 로드
+if [ -f "$SCRIPT_DIR/.env" ]; then
+  set -o allexport
+  source "$SCRIPT_DIR/.env"
+  set +o allexport
+
+  if ! helm repo list | grep -q 'nfs-subdir-external-provisioner'; then
+    helm repo add nfs-subdir-external-provisioner https://kubernetes-sigs.github.io/nfs-subdir-external-provisioner
+  fi
+  if ! helm status nfs-subdir-external-provisioner -n kube-system >/dev/null 2>&1; then
+    helm upgrade --install nfs-subdir-external-provisioner \
+      nfs-subdir-external-provisioner/nfs-subdir-external-provisioner \
+        -n kube-system \
+        --set nfs.server=${CICD_HOST} \
+        --set nfs.path=/mnt/nfs-subdir-external-provisioner
+  fi
+
+  kubectl apply -f ../kubectl/class-roles;
+
+  helm upgrade ${DATABASE_USER} . -f ./values-dev-custom.yaml \
+  --install -n ibs-dev --create-namespace --history-max 5 --wait --timeout 10m
+else
+  echo ".env 파일이 없습니다"
+  exit 1
+fi
