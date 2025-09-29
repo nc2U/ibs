19ba1e447627c30f46ba518f5a208f7f9844728e
Austin Kho <kori.susie@gmail.com>
2025-09-29 11:12:17 +0900

    empty views update

--- app/django/_excel/views.py
+++ app/django/_excel/views.py
@@ -1,25 +1,3 @@
-##############################################################################
-#
-# A Django view class to write an Excel file using the XlsxWriter
-# module.
-#
-# Copyright 2013-2020, John McNamara, jmcnamara@cpan.org
-#
-import datetime
-import io
-import json
+# from django.shortcuts import render
 
-import xlsxwriter
-import xlwt
-from django.core import serializers
-from django.db.models import Q, F, Sum, When, Case, PositiveBigIntegerField
-from django.http import HttpResponse
-from django.views.generic import View
-
-from cash.models import CashBook, ProjectCashBook
-from company.models import Company, Staff, Department, JobGrade, Position, DutyTitle
-from contract.models import Contract
-from docs.models import LawsuitCase
-from project.models import Project, ProjectOutBudget, Site, SiteOwner, SiteContract
-
-TODAY = datetime.date.today().strftime('%Y-%m-%d')
+# Create your views here.
