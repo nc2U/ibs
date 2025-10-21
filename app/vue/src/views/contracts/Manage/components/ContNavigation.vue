a58b9232da2049c12c3df2997ac86052193f5357
Austin Kho <kori.susie@gmail.com>
2025-10-21 21:57:33 +0900

    ContractManage update ing

--- app/vue/src/views/contracts/Manage/components/ContNavigation.vue
+++ app/vue/src/views/contracts/Manage/components/ContNavigation.vue
@@ -9,7 +9,6 @@ const [route, router] = [useRoute(), useRouter()]
 const contractor = computed(() => route.query.contractor)
 
 const isRegister = computed(() => route.name === '계약 상세 관리')
-// const isContorInfo = computed(() => route.name === '계약자 정보 변경')
 const isSuccession = computed(() => route.name === '권리 의무 승계')
 const isRelease = computed(() => route.name === '계약 해지 관리')
 </script>
