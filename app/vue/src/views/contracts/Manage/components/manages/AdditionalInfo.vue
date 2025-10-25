6d522be8911b0e99f4a6c198a9f44daaef622f9a
Austin Kho <kori.susie@gmail.com>
2025-10-25 19:26:12 +0900

    contract manage update

--- app/vue/src/views/contracts/Manage/components/manages/AdditionalInfo.vue
+++ app/vue/src/views/contracts/Manage/components/manages/AdditionalInfo.vue
@@ -1,7 +1,12 @@
 <script lang="ts" setup>
+import { ref } from 'vue'
 import AddProofDocs from './AddProofDocs.vue'
 import AddPledgeDocs from './AddPledgeDocs.vue'
 import AddConsultation from './AddConsultation.vue'
+
+// 토글 상태
+const isProofDocsExpanded = ref(true)
+const isConsultationExpanded = ref(true)
 </script>
 
 <template>
@@ -12,12 +17,21 @@ import AddConsultation from './AddConsultation.vue'
         <div>
           <strong>구비서류 제출 현황</strong>
         </div>
+        <v-btn
+          size="x-small"
+          :icon="isProofDocsExpanded ? 'mdi-chevron-up' : 'mdi-chevron-down'"
+          variant="text"
+          @click="isProofDocsExpanded = !isProofDocsExpanded"
+        />
       </div>
     </CCardHeader>
 
-    <AddProofDocs />
+    <CCollapse :visible="isProofDocsExpanded">
+      <!-- 탭 -->
+      <AddProofDocs />
 
-    <AddPledgeDocs />
+      <AddPledgeDocs />
+    </CCollapse>
   </CCard>
 
   <!-- 상담 내역 카드 -->
@@ -27,8 +41,16 @@ import AddConsultation from './AddConsultation.vue'
         <div>
           <strong>상담 내역</strong>
         </div>
+        <v-btn
+          size="x-small"
+          :icon="isConsultationExpanded ? 'mdi-chevron-up' : 'mdi-chevron-down'"
+          variant="text"
+          @click="isConsultationExpanded = !isConsultationExpanded"
+        />
       </div>
     </CCardHeader>
-    <AddConsultation />
+    <CCollapse :visible="isConsultationExpanded">
+      <AddConsultation />
+    </CCollapse>
   </CCard>
 </template>
