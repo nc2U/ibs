f1a8dc5f447836c5ea4e11a6ff58c756e8a96b6a
Austin Kho <kori.susie@gmail.com>
2025-10-29 14:59:06 +0900

    payment info update

--- app/django/contract/exports/pdf.py
+++ app/django/contract/exports/pdf.py
@@ -18,11 +18,9 @@ TODAY = date.today()
 
 class PdfExportCertOccupancy(View):
 
-    @staticmethod
-    def get(request):
+    def get(self, request):
         context = dict()
 
-        project = request.GET.get('project')  # 프로젝트 ID
         # 계약 건 객체
         cont_id = request.GET.get('contract')
         context['contract'] = contract = get_contract(cont_id)
@@ -32,6 +30,9 @@ class PdfExportCertOccupancy(View):
         pub_date = request.GET.get('date', None)
         pub_date = datetime.strptime(pub_date, '%Y-%m-%d').date() if pub_date else TODAY
         context['pub_date'] = pub_date
+
+        # 사용자정보
+        context['user'] = request.user.username
 
         # ----------------------------------------------------------------
 
@@ -70,6 +71,15 @@ class PdfExportCertOccupancy(View):
         else:
             html_string = f'<html><head>{landscape_css}</head><body>{html_string}</body></html>'
 
+        # 동적 텍스트 오버레이 추가
+        overlay_text = self.generate_text_overlay(context)
+
+        # HTML 문서의 body 끝에 오버레이 텍스트 추가
+        if '</body>' in html_string:
+            html_string = html_string.replace('</body>', f'{overlay_text}</body>')
+        else:
+            html_string = html_string + overlay_text
+
         html = HTML(string=html_string)
         html.write_pdf(target='/tmp/mypdf.pdf')
 
@@ -78,3 +88,87 @@ class PdfExportCertOccupancy(View):
             response = HttpResponse(pdf, content_type='application/pdf')
             response['Content-Disposition'] = f'attachment; filename="cert-occupancy.pdf"'
             return response
+
+    @staticmethod
+    def generate_text_overlay(context):
+        """이미지 위에 텍스트 오버레이 생성"""
+        contract = context.get('contract')
+        pub_date_obj = context.get('pub_date')
+        pub_date = pub_date_obj.strftime(f'%Y{"&nbsp;"*8}%m{"&nbsp;"*8}%d') if pub_date_obj else ""
+        user = context.get('user')
+
+        if not contract:
+            return ""
+
+        # 프로젝트 정보
+        project = contract.project
+        project_name = project.name if project else ""
+        project_location = project.location if project else ""
+
+        # 동호수 정보
+        unit_info = ""
+        try:
+            if hasattr(contract, 'key_unit') and contract.key_unit:
+                unit_info = str(contract.key_unit.houseunit) if hasattr(contract.key_unit, 'houseunit') else ""
+        except:
+            unit_info = ""
+
+        # 텍스트 오버레이 HTML 생성 (예시 좌표 - 실제 좌표는 조정 필요)
+        overlay_html = f"""
+        <!-- 동적 텍스트 오버레이 -->
+        <div style="position: absolute; top: 187px; left: 150px; font-size: 12px; z-index: 10;">
+            {project_name}
+        </div>
+        <div style="position: absolute; top: 187px; left: 476px; font-size: 12px; z-index: 10;">
+            {project_name}
+        </div>
+        <div style="position: absolute; top: 187px; left: 804px; font-size: 12px; z-index: 10;">
+            {project_name}
+        </div>
+        
+        <div style="position: absolute; top: 217px; left: 150px; font-size: 9px; z-index: 10;">
+            {project_location}
+        </div>
+        <div style="position: absolute; top: 217px; left: 476px; font-size: 9px; z-index: 10;">
+            {project_location}
+        </div>
+        <div style="position: absolute; top: 217px; left: 804px; font-size: 9px; z-index: 10;">
+            {project_location}
+        </div>
+        
+        <div style="position: absolute; top: 247px; left: 150px; font-size: 12px; z-index: 10;">
+            {unit_info}
+        </div>
+        <div style="position: absolute; top: 247px; left: 476px; font-size: 12px; z-index: 10;">
+            {unit_info}
+        </div>
+        <div style="position: absolute; top: 247px; left: 804px; font-size: 12px; z-index: 10;">
+            {unit_info}
+        </div>
+        
+        <div style="position: absolute; top: 518px; left: 180px; font-size: 12px; z-index: 10;">
+            {pub_date}
+        </div>
+        <div style="position: absolute; top: 518px; left: 506px; font-size: 12px; z-index: 10;">
+            {pub_date}
+        </div>
+        <div style="position: absolute; top: 518px; left: 834px; font-size: 12px; z-index: 10;">
+            {pub_date}
+        </div>
+        
+        <div style="position: absolute; top: 600px; left: 530px; font-size: 12px; z-index: 10;">
+            {project_name}
+        </div>
+        <div style="position: absolute; top: 600px; left: 850px; font-size: 12px; z-index: 10;">
+            {project_name}
+        </div>
+        
+        <div style="position: absolute; top: 642px; left: 530px; font-size: 12px; z-index: 10;">
+            {user}
+        </div>
+        <div style="position: absolute; top: 642px; left: 850px; font-size: 12px; z-index: 10;">
+            {user}
+        </div>
+        """
+
+        return overlay_html
