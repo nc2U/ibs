7447320452dff1674a765368ea07deffa241eaf7
Austin Kho <kori.susie@gmail.com>
2025-10-29 18:15:18 +0900

    import optimization

--- app/django/project/admin.py
+++ app/django/project/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from django.contrib.humanize.templatetags.humanize import intcomma
-from django.utils.html import format_html
 from import_export.admin import ImportExportMixin
 
 from .models import (Project, ProjectIncBudget, ProjectOutBudget, Site, SiteInfoFile,
