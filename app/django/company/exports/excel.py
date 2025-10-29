6a07cdc6117d55fb5786da0c5b1b69ee6d52899b
Austin Kho <kori.susie@gmail.com>
2025-10-29 23:47:51 +0900

    company excel.py update

--- app/django/company/exports/excel.py
+++ app/django/company/exports/excel.py
@@ -5,11 +5,11 @@ Company Excel Export Views
 """
 import datetime
 import io
+import json
 
 import xlsxwriter
+from django.core import serializers
 from django.db.models import Q
-from rest_framework import serializers
-from rest_framework.utils import json
 
 from _excel.mixins import ExcelExportMixin
 from company.models import Company, Staff, Department, JobGrade, Position, DutyTitle
@@ -157,7 +157,7 @@ class ExportDeparts(ExcelExportMixin):
 
     def get(self, request):
         # 워크북 생성
-        output, workbook, worksheet = self.create_workbook('계좌별_자금현황')
+        output, workbook, worksheet = self.create_workbook('부서_정보')
 
         # data start --------------------------------------------- #
         company = Company.objects.get(pk=request.GET.get('company'))
@@ -253,23 +253,17 @@ class ExportDeparts(ExcelExportMixin):
 class ExportPositions(ExcelExportMixin):
     """직위 목록 정보"""
 
-    @staticmethod
-    def get(request):
-        # Create an in-memory output file for the new workbook.
-        output = io.BytesIO()
-
-        # Even though the final file will be in memory the module uses temp
-        # files during assembly for efficiency. To avoid this on servers that
-        # don't allow temp files, for example the Google APP Engine, set the
-        # 'in_memory' Workbook() constructor option as shown in the docs.
-        workbook = xlsxwriter.Workbook(output)
-        worksheet = workbook.add_worksheet('직위 정보')
-
-        worksheet.set_default_row(20)  # 기본 행 높이
+    def get(self, request):
+        # 워크북 생성
+        output, workbook, worksheet = self.create_workbook('직위_정보')
 
         # data start --------------------------------------------- #
         company = Company.objects.get(pk=request.GET.get('company'))
         com_name = company.name.replace('주식회사 ', '(주)')
+
+        # 포맷 생성
+        title_format = self.create_title_format(workbook)
+        h_format = self.create_header_format(workbook)
 
         # title_list
         header_src = [[],
@@ -289,11 +283,7 @@ class ExportPositions(ExcelExportMixin):
         # 1. Title
         row_num = 0
         worksheet.set_row(row_num, 50)
-        title_format = workbook.add_format()
-        title_format.set_bold()
-        title_format.set_font_size(18)
-        title_format.set_align('vcenter')
-        worksheet.merge_range(row_num, 0, row_num, len(header_src) - 1, com_name + ' 직원 정보 목록', title_format)
+        worksheet.merge_range(row_num, 0, row_num, len(header_src) - 1, com_name + ' 직위 정보 목록', title_format)
 
         # 2. Pre Header - Date
         row_num = 1
@@ -303,13 +293,6 @@ class ExportPositions(ExcelExportMixin):
         # 3. Header - 1
         row_num = 2
         worksheet.set_row(row_num, 20, workbook.add_format({'bold': True}))
-
-        h_format = workbook.add_format()
-        h_format.set_bold()
-        h_format.set_border()
-        h_format.set_align('center')
-        h_format.set_align('vcenter')
-        h_format.set_bg_color('#eeeeee')
 
         # Adjust the column width.
         for i, col_width in enumerate(widths):
@@ -328,12 +311,6 @@ class ExportPositions(ExcelExportMixin):
         json_data = serializers.serialize('json', obj_list)
         data = [i['fields'] for i in json.loads(json_data)]
         # data = obj_list.values_list(*params)
-
-        b_format = workbook.add_format()
-        b_format.set_border()
-        b_format.set_align('center')
-        b_format.set_align('vcenter')
-        b_format.set_num_format('yyyy-mm-dd')
 
         body_format = {
             'border': True,
@@ -372,9 +349,9 @@ class ExportPositions(ExcelExportMixin):
         # data finish -------------------------------------------- #
 
         # Close the workbook before sending the data.
-        filename = request.GET.get('filename')
-        filename = filename if filename else f'{TODAY}-positions.xlsx'
-        return ExcelExportMixin.create_response(output, workbook, filename)
+        filename = request.GET.get('filename') or 'positions'
+        filename = f'{filename}-{TODAY}'
+        return self.create_response(output, workbook, filename)
 
 
 class ExportDuties(ExcelExportMixin):
