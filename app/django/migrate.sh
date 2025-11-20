1fe869989d0ce45c2d6f67a31ce952b2dfaccc09
Austin Kho <kori.susie@gmail.com>
2025-11-20 10:40:31 +0900

    migrate script update

--- app/django/migrate.sh
+++ app/django/migrate.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-python manage.py makemigrations accounts board book cash company contract docs ibs items notice payment project work
+python manage.py makemigrations accounts board book cash company contract docs ibs items ledger notice payment project work
 python manage.py migrate --database=default
