5c1f08d4ea5f5fe5dec7853d6ba503512c07c77a
Austin Kho <kori.susie@gmail.com>
2025-11-24 22:21:35 +0900

    sidebar nav update

--- app/vue/src/views/proLedger/_menu/headermixin.ts
+++ app/vue/src/views/proLedger/_menu/headermixin.ts
@@ -1,2 +1,2 @@
-export const pageTitle = 'PR 자금 관리'
-export const navMenu = ['PR 자금 현황', 'PR 출납 내역', '운영 비용 내역']
+export const pageTitle = 'PR 회계 관리'
+export const navMenu = ['PR 자금 현황1', 'PR 출납 내역1', '운영 비용 내역1']
