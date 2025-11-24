bf000c91c5a7a9d6dccdabfc2c666b85e9564165
Austin Kho <kori.susie@gmail.com>
2025-11-24 23:00:34 +0900

    Ledger AuthGuard update

--- app/vue/src/components/AuthGuard/ComLedgerAuthGuard.vue
+++ app/vue/src/components/AuthGuard/ComLedgerAuthGuard.vue
@@ -10,7 +10,7 @@ const isLoading = computed(() => account.userInfo === null || account.userInfo =
 const hasAuth = computed(
   () =>
     account.userInfo?.is_superuser ||
-    (account.userInfo?.staffauth && account.userInfo.staffauth?.project_ledger > '0'),
+    (account.userInfo?.staffauth && account.userInfo.staffauth?.company_ledger > '0'),
 )
 </script>
 
