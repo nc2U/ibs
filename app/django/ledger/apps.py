4dbd57389318139a236abf1fed3ea22cf589206c
Austin Kho <kori.susie@gmail.com>
2025-12-17 13:24:46 +0900

    ledger signals.py update

--- app/django/ledger/apps.py
+++ app/django/ledger/apps.py
@@ -5,3 +5,6 @@ class LedgerConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'ledger'
     verbose_name = '회계 원장 정보 [LEDGER]'
+
+    def ready(self):
+        import ledger.signals
