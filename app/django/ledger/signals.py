cb3010d0cd04585d054305f0cca29283074abd42
Austin Kho <kori.susie@gmail.com>
2025-12-30 23:05:25 +0900

    ledger signals update

--- app/django/ledger/signals.py
+++ app/django/ledger/signals.py
@@ -2,7 +2,7 @@ from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 
 from _utils.slack_notifications import send_slack_notification
-from .models import CompanyBankTransaction, ProjectBankTransaction, CompanyAccountingEntry, ProjectAccountingEntry
+from .models import CompanyBankTransaction, ProjectBankTransaction
 from .services.sync_payment_contract import is_bulk_import_active
 
 
@@ -54,55 +54,3 @@ def notify_project_bank_transaction_delete(sender, instance, **kwargs):
         return
 
     send_slack_notification(instance, "삭제", getattr(instance, 'creator', None))
-
-
-# --- is_balanced 자동 업데이트를 위한 시그널 ---
-
-def _update_bank_transaction_balance(entry_instance):
-    """
-    AccountingEntry 인스턴스에 연결된 BankTransaction의 is_balanced를 업데이트합니다.
-    """
-    # Skip updates during bulk import to avoid performance issues
-    if is_bulk_import_active():
-        return
-
-    transaction = entry_instance.related_transaction
-    if transaction:
-        # is_balanced 값 계산
-        try:
-            validation_result = transaction.validate_accounting_entries()
-            new_is_balanced = validation_result['is_valid']
-
-            # 값이 실제로 변경된 경우에만 업데이트 (Slack 알림 중복 방지)
-            if transaction.is_balanced != new_is_balanced:
-                transaction.is_balanced = new_is_balanced
-                # update_fields 사용하여 is_balanced만 업데이트하고 signal 트리거 방지
-
-                # 거래 타입에 따라 적절한 signal handler disconnect/reconnect
-                if isinstance(transaction, CompanyBankTransaction):
-                    post_save.disconnect(notify_bank_transaction_change, sender=CompanyBankTransaction)
-                    transaction.save(update_fields=['is_balanced'])
-                    post_save.connect(notify_bank_transaction_change, sender=CompanyBankTransaction)
-                elif isinstance(transaction, ProjectBankTransaction):
-                    post_save.disconnect(notify_project_bank_transaction_change, sender=ProjectBankTransaction)
-                    transaction.save(update_fields=['is_balanced'])
-                    post_save.connect(notify_project_bank_transaction_change, sender=ProjectBankTransaction)
-        except (AttributeError, NotImplementedError):
-            # validate_accounting_entries가 구현되지 않은 경우 무시
-            pass
-
-
-@receiver(post_save, sender=CompanyAccountingEntry)
-@receiver(post_save, sender=ProjectAccountingEntry)
-def on_accounting_entry_save(sender, instance, created, raw=False, **kwargs):
-    """CompanyAccountingEntry 또는 ProjectAccountingEntry 저장 후 호출됩니다."""
-    if raw:
-        return
-    _update_bank_transaction_balance(instance)
-
-
-@receiver(post_delete, sender=CompanyAccountingEntry)
-@receiver(post_delete, sender=ProjectAccountingEntry)
-def on_accounting_entry_delete(sender, instance, **kwargs):
-    """CompanyAccountingEntry 또는 ProjectAccountingEntry 삭제 후 호출됩니다."""
-    _update_bank_transaction_balance(instance)
