f8a3aaa3ebd8399c736f2bb6906e99059ca26f3d
Austin Kho <kori.susie@gmail.com>
2026-05-24 15:09:07 +0900

    wiki & file module remove

--- app/vue/src/views/_Work/components/Header/components/HeaderNav.vue
+++ app/vue/src/views/_Work/components/Header/components/HeaderNav.vue
@@ -7,7 +7,7 @@ defineProps({
   menus: { type: Array, required: true },
 })
 
-const workManager = inject<ComputedRef<boolean>>('workManager')
+const workManager = inject('workManager', false)
 
 const [route, router] = [useRoute(), useRouter()]
 
@@ -18,24 +18,24 @@ const getTitle = (title: string) => title.replace(/[() ]/gim, '')
 </script>
 
 <template>
-  <CNav v-if="!route.path.startsWith('/manage') || workManager?.value" variant="tabs" class="mb-0 pl-4">
+  <CNav v-if="!route.path.startsWith('/manage') || workManager" variant="tabs" class="mb-0 pl-4">
     <CDropdown v-if="route.params['projId']">
       <CDropdownToggle :color="isDark ? 'dark' : 'light'" />
       <CDropdownMenu>
         <CDropdownItem @click="router.push({ name: '(업무) - 추가' })">
           새 업무 만들기
         </CDropdownItem>
-        <CDropdownItem v-if="workManager?.value" @click="router.push({ name: '(설정) - 범주추가' })">
+        <CDropdownItem v-if="workManager" @click="router.push({ name: '(설정) - 범주추가' })">
           새 업무 범주
         </CDropdownItem>
-        <CDropdownItem v-if="workManager?.value" @click="router.push({ name: '(로드맵) - 추가' })">
+        <CDropdownItem v-if="workManager" @click="router.push({ name: '(로드맵) - 추가' })">
           새 버전
         </CDropdownItem>
-        <CDropdownItem v-if="workManager?.value" @click="router.push({ name: '(소요시간) - 추가' })">
+        <CDropdownItem v-if="workManager" @click="router.push({ name: '(소요시간) - 추가' })">
           작업시간 기록
         </CDropdownItem>
-        <CDropdownItem v-if="workManager?.value" disabled>새 뉴스</CDropdownItem>
-        <CDropdownItem v-if="workManager?.value" disabled>새 문서</CDropdownItem>
+        <CDropdownItem v-if="workManager" disabled>새 뉴스</CDropdownItem>
+        <CDropdownItem v-if="workManager" disabled>새 문서</CDropdownItem>
       </CDropdownMenu>
     </CDropdown>
     <CNavItem v-for="(menu, i) in menus" :key="i">
