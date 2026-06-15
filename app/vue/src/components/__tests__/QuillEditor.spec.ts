ca76fc25ee5ea06a9d53ed74252fc49611768b4e
Austin Kho <kori.susie@gmail.com>
2026-06-15 19:21:17 +0900

    test update

--- app/vue/src/components/__tests__/QuillEditor.spec.ts
+++ app/vue/src/components/__tests__/QuillEditor.spec.ts
@@ -1,15 +1,15 @@
 import { describe, expect, it } from 'vitest'
-import { mount } from '@vue/test-utils'
+import { mount, flushPromises } from '@vue/test-utils'
 
 import QuillEditor from '../QuillEditor/index.vue'
 
 describe('QuillEditor Component Test', () => {
-  it('Quild editor test', () => {
+  it('Quild editor test', async () => {
     const wrapper = mount(QuillEditor)
+    await flushPromises()
 
     expect(wrapper.find('.ql-container').exists()).toBeTruthy()
     expect(wrapper.find('.ql-editor').exists()).toBeTruthy()
-    expect(wrapper.find('.ql-clipboard').exists()).toBeTruthy()
     expect(wrapper.find('.ql-tooltip').exists()).toBeTruthy()
     expect(wrapper.find('a.ql-preview').exists()).toBeTruthy()
     expect(wrapper.find('input[type=text]').exists()).toBeTruthy()
