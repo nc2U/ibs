f84b66791f0dd933fac3cc430e2502087d10ac44
Austin Kho <kori.susie@gmail.com>
2026-06-21 22:41:18 +0900

    meeting obj update

--- app/vue/src/views/_Work/Manages/Meetings/components/MeetingObj.vue
+++ app/vue/src/views/_Work/Manages/Meetings/components/MeetingObj.vue
@@ -54,7 +54,7 @@ const downloadPdf = (event: Event) => {
     </v-chip>
   </CTableDataCell>
   <CTableDataCell>{{ meeting.category_desc?.name }}</CTableDataCell>
-  <CTableDataCell class="text-left">
+  <CTableDataCell class="text-left" :class="{ closed: meeting.status === '3' }">
     <router-link to="">{{ meeting.title }}</router-link>
   </CTableDataCell>
   <CTableDataCell>{{ meetingDate }}</CTableDataCell>
