909ed0cb8136421fd3804ddcebf8d2ece1e23d46
Austin Kho <kori.susie@gmail.com>
2026-07-02 03:31:12 +0900

    type error debug

--- app/vue/src/components/QuillEditor/index.vue
+++ app/vue/src/components/QuillEditor/index.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
+import type { PropType } from 'vue'
 import { QuillEditor } from '@vueup/vue-quill'
 import '@vueup/vue-quill/dist/vue-quill.snow.css'
 
 defineProps({
-  theme: { type: String, default: 'snow' },
+  theme: { type: String as PropType<'snow' | 'bubble'>, default: 'snow' },
   height: { type: Number, default: 300 },
   color: { type: String, default: 'white' },
   placeholder: { type: String, default: '내용을 입력하세요' },
