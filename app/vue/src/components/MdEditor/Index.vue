0aeb77a029c6577045b994e23746843d47b951a7
Austin Kho <kori.susie@gmail.com>
2026-07-04 01:46:43 +0900

    md-editor debug

--- app/vue/src/components/MdEditor/Index.vue
+++ app/vue/src/components/MdEditor/Index.vue
@@ -4,10 +4,14 @@ import { MdEditor } from 'md-editor-v3'
 import 'md-editor-v3/lib/style.css'
 import { computed } from 'vue'
 
-defineProps({
+const props = defineProps({
+  modelValue: { type: String, default: '' },
   codeTheme: { type: String, default: 'atom' },
   height: { type: Number, default: 250 },
+  placeholder: { type: String, default: '' },
 })
+
+const emit = defineEmits(['update:modelValue'])
 
 const store = useStore()
 const theme = computed(() => (store.theme === 'dark' ? 'dark' : 'light'))
@@ -15,11 +19,15 @@ const theme = computed(() => (store.theme === 'dark' ? 'dark' : 'light'))
 
 <template>
   <MdEditor
+    :modelValue="modelValue"
+    @update:modelValue="val => emit('update:modelValue', val)"
     language="en-US"
     :codeTheme="codeTheme"
     :toolbarsExclude="['github']"
-    :style="`height: ${250}px`"
+    :style="`height: ${height}px`"
     :theme="theme"
+    :placeholder="placeholder"
+    :noMermaid="false"
   />
 </template>
 
