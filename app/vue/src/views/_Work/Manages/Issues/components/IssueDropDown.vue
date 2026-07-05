9be855a13b56618c3737794879e812821750cf21
Austin Kho <kori.susie@gmail.com>
2026-07-06 00:29:40 +0900

    work issue dropdown update

--- app/vue/src/views/_Work/Manages/Issues/components/IssueDropDown.vue
+++ app/vue/src/views/_Work/Manages/Issues/components/IssueDropDown.vue
@@ -1,8 +1,8 @@
 <script lang="ts" setup>
 import { computed, onBeforeMount, type PropType, ref } from 'vue'
 import { useAccount } from '@/store/pinia/account.ts'
+import { useIssue } from '@/store/pinia/work_issue.ts'
 import { useRouter } from 'vue-router'
-import { usePerms } from '@/composables/usePerms.ts'
 import type { Issue, SimpleIssue } from '@/store/types/work_issue.ts'
 
 const props = defineProps({
@@ -13,10 +13,11 @@ const emit = defineEmits(['watch-control'])
 
 const router = useRouter()
 
-const { can, PERM } = usePerms()
-
 const accStore = useAccount()
 const userInfo = computed(() => accStore.userInfo)
+
+const issueStore = useIssue()
+const statusList = computed(() => issueStore.statusList)
 
 const isWatcher = ref(false)
 
@@ -29,84 +30,159 @@ const watchControl = () => {
   isWatcher.value = !isWatcher.value
 }
 
+const changeStatus = (statusPk: number) => {
+  const currentStatusPk =
+    typeof props.issue.status === 'object' ? props.issue.status.pk : props.issue.status
+  if (currentStatusPk !== statusPk) {
+    issueStore.patchIssue(props.issue.pk, { status: statusPk })
+  }
+}
+
 onBeforeMount(() => (isWatcher.value = isCumputedWatcher.value as any))
 </script>
 
 <template>
   <span>
-    <CDropdown color="secondary" variant="input-group" placement="bottom-end">
-      <CDropdownToggle :caret="false" color="light" variant="ghost" size="sm" shape="rounded-pill">
-        <v-icon icon="mdi-dots-horizontal" class="pointer" color="grey-darken-1" />
-        <v-tooltip activator="parent" location="top">Actions</v-tooltip>
-      </CDropdownToggle>
-      <CDropdownMenu>
-        <CDropdownItem
-          class="form-text"
-          @click="
-            router.push({
-              name: '(업무) - 보기',
-              params: { projId: issue.project.slug, issueId: issue.pk },
-              query: { edit: '1' },
-            })
-          "
-        >
-          <v-icon icon="mdi-pencil" color="amber" size="sm" />
-          <span class="text-primary ml-1">편집</span>
-        </CDropdownItem>
-        <CDropdownItem class="form-text" disabled>
-          <v-icon size="sm" />
-          <span class="ml-1">상태</span>
-        </CDropdownItem>
-        <CDropdownItem class="form-text" disabled>
-          <v-icon size="sm" />
-          <span class="ml-1">유형</span>
-        </CDropdownItem>
-        <CDropdownItem class="form-text" disabled>
-          <v-icon size="sm" />
-          <span class="ml-1">우선순위</span>
-        </CDropdownItem>
-        <CDropdownItem class="form-text" disabled>
-          <v-icon size="sm" />
-          <span class="ml-1">담당자</span>
-        </CDropdownItem>
-        <CDropdownItem class="form-text" disabled>
-          <v-icon size="sm" />
-          <span class="ml-1">진척도</span>
-        </CDropdownItem>
-        <CDropdownItem class="form-text" disabled>
-          <v-icon size="sm" />
-          <span class="ml-1">업무 관람자</span>
-        </CDropdownItem>
-        <CDropdownItem class="form-text" @click="watchControl">
-          <v-icon icon="mdi-star" :color="isWatcher ? 'amber' : 'secondary'" size="sm" />
-          <span class="text-primary ml-1">{{ isWatcher ? '관심끄기' : '지켜보기' }}</span>
-        </CDropdownItem>
-        <CDropdownItem
-          class="form-text"
-          @click="
-            router.push({
-              name: '(업무) - 추가',
-              params: { projId: issue.project.slug },
-              query: { parent: issue.pk, tracker: issue.tracker.pk },
-            })
-          "
-        >
-          <v-icon icon="mdi-plus-circle" color="success" size="sm" />
-          <span class="text-primary ml-1">하위 업무 추가</span>
-        </CDropdownItem>
-        <CDropdownItem class="form-text" disabled>
-          <v-icon icon="mdi-link-edit" color="secondary" size="sm" />
-          <span class="ml-1">링크복사</span>
-        </CDropdownItem>
-        <CDropdownItem class="form-text" disabled>
-          <v-icon icon="mdi-content-copy" color="secondary" size="sm" />
-          <span class="ml-1">복사</span>
-        </CDropdownItem>
-        <CDropdownItem class="form-text" disabled>
-          <v-icon icon="mdi-trash-can-outline" color="secondary" size="sm" />
-          <span class="ml-1">업무 삭제</span>
-        </CDropdownItem>
-      </CDropdownMenu>
-    </CDropdown>
+    <v-btn icon variant="text" size="x-small" color="grey-darken-1">
+      <v-icon icon="mdi-dots-horizontal" />
+      <v-tooltip activator="parent" location="top">Actions</v-tooltip>
+      <v-menu activator="parent" location="bottom end" transition="scale-transition">
+        <v-list density="compact" class="py-1">
+          <v-list-item
+            min-height="30px"
+            class="py-0"
+            @click="
+              router.push({
+                name: '(업무) - 보기',
+                params: { projId: issue.project.slug, issueId: issue.pk },
+                query: { edit: '1' },
+              })
+            "
+          >
+            <template v-slot:prepend>
+              <v-icon icon="mdi-pencil" color="amber" size="small" class="mr-n2" />
+            </template>
+            <v-list-item-title class="text-caption">편집</v-list-item-title>
+          </v-list-item>
+
+          <v-menu open-on-hover location="end" offset="5">
+            <template v-slot:activator="{ props: menuProps }">
+              <v-list-item
+                v-bind="menuProps"
+                append-icon="mdi-chevron-right"
+                min-height="28px"
+                class="py-0"
+              >
+                <template v-slot:prepend>
+                  <v-icon icon="mdi-list-status" color="primary" size="small" class="mr-n2" />
+                </template>
+                <v-list-item-title class="text-caption">상태</v-list-item-title>
+              </v-list-item>
+            </template>
+            <v-list density="compact" class="py-1">
+              <v-list-item
+                v-for="status in statusList"
+                :key="status.pk"
+                :active="
+                  (typeof issue.status === 'object' ? issue.status.pk : issue.status) === status.pk
+                "
+                min-height="28px"
+                class="py-0"
+                @click="changeStatus(status.pk)"
+              >
+                <v-list-item-title class="text-caption">{{ status.name }}</v-list-item-title>
+              </v-list-item>
+            </v-list>
+          </v-menu>
+
+          <v-list-item disabled min-height="28px" class="py-0">
+            <template v-slot:prepend>
+              <v-icon size="small" class="mr-n2" />
+            </template>
+            <v-list-item-title class="text-caption">유형</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item disabled min-height="28px" class="py-0">
+            <template v-slot:prepend>
+              <v-icon size="small" class="mr-n2" />
+            </template>
+            <v-list-item-title class="text-caption">우선순위</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item disabled min-height="28px" class="py-0">
+            <template v-slot:prepend>
+              <v-icon size="small" class="mr-n2" />
+            </template>
+            <v-list-item-title class="text-caption">담당자</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item disabled min-height="28px" class="py-0">
+            <template v-slot:prepend>
+              <v-icon size="small" class="mr-n2" />
+            </template>
+            <v-list-item-title class="text-caption">진척도</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item disabled min-height="28px" class="py-0">
+            <template v-slot:prepend>
+              <v-icon size="small" class="mr-n2" />
+            </template>
+            <v-list-item-title class="text-caption">업무 관람자</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item @click="watchControl" min-height="28px" class="py-0">
+            <template v-slot:prepend>
+              <v-icon
+                icon="mdi-star"
+                :color="isWatcher ? 'amber' : 'secondary'"
+                size="small"
+                class="mr-n2"
+              />
+            </template>
+            <v-list-item-title class="text-caption">
+              {{ isWatcher ? '관심끄기' : '지켜보기' }}
+            </v-list-item-title>
+          </v-list-item>
+
+          <v-list-item
+            min-height="28px"
+            class="py-0"
+            @click="
+              router.push({
+                name: '(업무) - 추가',
+                params: { projId: issue.project.slug },
+                query: { parent: issue.pk, tracker: issue.tracker.pk },
+              })
+            "
+          >
+            <template v-slot:prepend>
+              <v-icon icon="mdi-plus-circle" color="success" size="small" class="mr-n2" />
+            </template>
+            <v-list-item-title class="text-caption">하위 업무 추가</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item disabled min-height="28px" class="py-0">
+            <template v-slot:prepend>
+              <v-icon icon="mdi-link-edit" color="secondary" size="small" class="mr-n2" />
+            </template>
+            <v-list-item-title class="text-caption">링크복사</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item disabled min-height="28px" class="py-0">
+            <template v-slot:prepend>
+              <v-icon icon="mdi-content-copy" color="secondary" size="small" class="mr-n2" />
+            </template>
+            <v-list-item-title class="text-caption">복사</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item disabled min-height="28px" class="py-0">
+            <template v-slot:prepend>
+              <v-icon icon="mdi-trash-can-outline" color="secondary" size="small" class="mr-n2" />
+            </template>
+            <v-list-item-title class="text-caption">업무 삭제</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    </v-btn>
   </span>
 </template>
