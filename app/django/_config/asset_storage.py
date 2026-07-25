283500ad40b76546469bd0e0eb2bd13cd79804fe
Austin Kho <kori.susie@gmail.com>
2026-07-25 23:46:25 +0900

    helm script update

--- app/django/_config/asset_storage.py
+++ app/django/_config/asset_storage.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage
 
 
@@ -7,3 +8,4 @@ class MediaStorage(S3Boto3Storage):
     # Private 버킷: Pre-signed URL로 임시 접근 (기본값 True)
     # URL 유효기간은 settings.py의 AWS_QUERYSTRING_EXPIRE로 제어
     querystring_auth = True
+    custom_domain = getattr(settings, 'AWS_S3_CUSTOM_DOMAIN', None)
