31f287bb9db94fa6b2c480388053f617b0303365
Austin Kho <kori.susie@gmail.com>
2026-07-30 10:53:16 +0900

    contract model update

--- app/django/contract/models.py
+++ app/django/contract/models.py
@@ -425,7 +425,9 @@ class Contractor(models.Model):
                                 null=True, blank=True, verbose_name='등록자')
 
     def __str__(self):
-        return f'{self.name}({self.contract.serial_number if self.contract else self.prev_contract.serial_number})'
+        contract = self.contract or self.prev_contract
+        serial = contract.serial_number if contract else '계약없음'
+        return f'{self.name}({serial})'
 
     @property
     def contractoraddress(self):
