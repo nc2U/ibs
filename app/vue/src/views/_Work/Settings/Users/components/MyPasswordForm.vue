af1bc3e1466a436b5418a8cbd6e11fd94a50a1e0
Austin Kho <kori.susie@gmail.com>
2026-08-04 17:38:23 +0900

    my password manage update

--- app/vue/src/views/_Work/Settings/Users/components/MyPasswordForm.vue
+++ app/vue/src/views/_Work/Settings/Users/components/MyPasswordForm.vue
@@ -3,6 +3,7 @@ import { ref, reactive, computed } from 'vue'
 import { useRouter } from 'vue-router'
 import { useAccount } from '@/store/pinia/account'
 import AlertModal from '@/components/Modals/AlertModal.vue'
+import TextButton from '@/views/_Work/components/atomics/TextButton.vue'
 
 const refAlertModal = ref()
 
@@ -45,6 +46,17 @@ const onSubmit = async (event: Event) => {
   <CRow class="py-2">
     <CCol class="mb-2">
       <span class="h5 mr-2"> 비밀번호 변경 </span>
+    </CCol>
+
+    <CCol class="text-right form-text">
+      <span class="mr-2">
+        <TextButton
+          name="내 계정"
+          :to="{ name: '사용자 - 내 계정' }"
+          icon="mdi-pencil"
+          icon-color="amber"
+        />
+      </span>
     </CCol>
   </CRow>
 
