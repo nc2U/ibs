327fdc1aab038fb6e6a495ca4403950cbbcd3872
nc2U <nc2U@users.noreply.github.com>
2026-08-07 06:32:11 +0000

    deploy: 58957b6611cd4c8acae5704b512f0c8abc9640a3

---
+++ assets/chunks/swimlanesDiagram-ULZ7WXOC.Dolyvp6C.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-UKHOOZJN.CJtFao_g.js";import{_ as a}from"../app.DIFsBc_T.js";import"./chunk-5VM5RSS4.BzrIrnnx.js";import"./chunk-XXDRQBXY.CKjj8F84.js";import"./chunk-KBJHAD2P.CGtfOnKW.js";import"./chunk-2GRJ4B5K.DhIdrcO0.js";import"./channel.wd9Lk0bs.js";import"./framework.CZGNF-7T.js";import"./theme.BfRxSZg-.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
