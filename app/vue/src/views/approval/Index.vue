f9fd2efda4d27fa42bd193c671aadea72d9c7b77
Austin Kho <kori.susie@gmail.com>
2026-08-18 13:19:28 +0900

    approval index page update

---
+++ app/vue/src/views/approval/Index.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { onBeforeMount, ref } from 'vue'
+import { navMenu, pageTitle } from '@/views/comDocs/_menu/headermixin'
+import { useRoute, useRouter } from 'vue-router'
+
+const [route, router] = [useRoute(), useRouter()]
+
+const comSelect = async (target: number | null, skipClearQuery = false) => {
+  if (!!target) {
+  }
+}
+
+const loading = ref(true)
+onBeforeMount(async () => {
+  loading.value = false
+})
+</script>
+
+<template>
+  <ComDocsAuthGuard>
+    <Loading v-model:active="loading" />
+    <ContentHeader
+      :page-title="pageTitle"
+      :nav-menu="navMenu"
+      selector="CompanySelect"
+      @com-select="comSelect"
+    />
+
+    <ContentBody>
+      <CCardBody class="pb-5">전자 결제 페이지</CCardBody>
+    </ContentBody>
+  </ComDocsAuthGuard>
+</template>
